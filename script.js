eb9e3ba4840615312db7afc5d2176c037bd940fe
Carlo C <pbjunkiep3@yahoo.com>
2020-08-16 10:15:12 +1000

    functionality to add burgeris working

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+let burgerHolder = [];
+
 $("#burgerButton").on("click",(e) =>{ 
     e.preventDefault();
 
     let burgerInput = $("#burgerInput").val();
-    console.log(burgerInput);
+    burgerHolder.push(burgerInput);
+
+    //Create Burger Button
+    $("#left-side-list").empty();
+    for(let i = 0; i<burgerHolder.length; i++){
+        const btnTag = $("<button>");
+        btnTag.addClass("burgerButton");
+        btnTag.attr("burgerType",burgerHolder[i]);
+        btnTag.text(burgerHolder[i]);
+        $("#left-side-list").append(btnTag);
+    }
+
+
 })
 
-// const eatBurg = () => {
-//     console.log('eat Burg');
-// }
