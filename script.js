23788ed705a3de29b5be8e6702a9fec9fb383449
Carlo C <pbjunkiep3@yahoo.com>
2020-08-16 14:20:52 +1000

    added eat me functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let burgerHolder = [];
+let burgerEaten = [];
 
 $("#burgerButton").on("click",(e) =>{ 
     e.preventDefault();
@@ -6,16 +7,48 @@ $("#burgerButton").on("click",(e) =>{
     let burgerInput = $("#burgerInput").val();
     burgerHolder.push(burgerInput);
 
-    //Create Burger Button
+    createBurger();
+    
+})
+
+const createBurger = () => {
+
     $("#left-side-list").empty();
     for(let i = 0; i<burgerHolder.length; i++){
         const btnTag = $("<button>");
-        btnTag.addClass("burgerButton");
+        btnTag.addClass("addedBurger");
         btnTag.attr("burgerType",burgerHolder[i]);
         btnTag.text(burgerHolder[i]);
         $("#left-side-list").append(btnTag);
     }
+}
 
+$(document).on("click", ".addedBurger", (e) =>{
+    eatDaBurger($(e.target).attr("burgerType"))
+});
 
-})
+const eatDaBurger = (eaten) => {
 
+    let burgToEat = eaten;
+    burgerEaten.push(burgToEat);
+
+    $("#right-side-list").empty();
+    for(let i = 0; i < burgerEaten.length; i++){
+
+            const btnTag = $("<strike>");
+            btnTag.addClass("addedBurger");
+            btnTag.attr("burgerType",burgerEaten[i]);
+            btnTag.text(burgerEaten[i]);
+            $("#right-side-list").append(btnTag);
+    }
+
+    for(let x = 0; x < burgerHolder.length; x++){
+        if(burgerHolder[x] === burgToEat){
+            burgerHolder.splice(burgerHolder[x],1);
+        }
+        createBurger();
+    }
+    
+}
+
+ 
