c9595e8e54c7f8984a8667d404923565aa377b53
Carlo C <pbjunkiep3@yahoo.com>
2020-08-23 08:24:49 +1000

    added seeds

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgerProject;
+USE burgerProject;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burger (name) VALUES ('Double Double');
+INSERT INTO burger (name) VALUES ('Cheese Burger');
