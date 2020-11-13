e757e79e715e28960a8685321cd5081ec3f1ccc3
Carlo C <pbjunkiep3@yahoo.com>
2020-11-13 19:39:09 +1100

    fixed server file

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,12 @@
+const connection = require("../config/connection.js");
+
+const printQuestion = (num)=> {
+    const arr = [];
+
+    for(let i=0; i<num; i++){
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
 
--- server.js
+++ server.js
@@ -4,76 +4,20 @@ const mysql = require("mysql");
 
 const app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
 
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extensions: true}));
 app.use(express.json());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+const exHandles = require("express-handlebars");
+
+app.engine("handlebars", exHandles({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "forgo-reason",
-  database: "burgerProject"
-});
+const routes = require("./controllers/burger.js");
 
-connection.connect((err) => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+app.use(routes);
 
-  console.log("connected as id " + connection.threadId);
-});
-
-// Use Handlebars to render the main index.html page with the plans in it.
-app.get("/", (req, res) => {
-  connection.query("SELECT * FROM burger;", (err, data) => {
-    if (err) {
-      return res.status(500).end();
-    }
-    console.log(data);
-    res.render("index", { name: data });
-  });
-});
-
-// Create a new burger
-app.post("/api/burger", (req, res) => {
-  connection.query("INSERT INTO burger (name) VALUES (?)", [req.body.burger], (err, result) => {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    // Send back the ID of the new plan
-    res.json({ id: result.insertId });
-    console.log({ id: result.insertId });
-  });
-});
-
-// Eat a Burger
-app.delete("/api/burger/:id", (req, res) => {
-  connection.query("DELETE FROM burger WHERE id = ?", [req.params.id], (err, result) => {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-    else if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, () => {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT,() => {
+  console.log("Listening PORT: " + PORT);
 });
